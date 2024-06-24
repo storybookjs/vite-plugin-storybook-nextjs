85056ce71257b6998d842f04d5f75c556c2ce8cb
Valentin Palkovic <valentin@chromatic.com>
2024-06-24 11:30:31 +0200

    Add examples to verify Next.js' style functionalities

--- example/.storybook/preview.ts
+++ example/.storybook/preview.ts
@@ -1,5 +1,13 @@
 import type { Preview } from "@storybook/react";
 
+import "./global.css";
+/**
+ * This external stylesheet import checks Next.js' external stylesheet support
+ * by referencing Bootstrap's css in ./src/app/components/BootstrapButton/BootstrapButton.tsx
+ * https://nextjs.org/docs/app/building-your-application/styling/css-modules#external-stylesheets
+ */
+import "bootstrap/dist/css/bootstrap.css";
+
 const preview: Preview = {
 	parameters: {
 		controls: {
