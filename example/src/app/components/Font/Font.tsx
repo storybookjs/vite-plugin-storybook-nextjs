a645bef2d50f849076bf108e1c43e9698963d4aa
Valentin Palkovic <valentin@chromatic.com>
2024-07-22 12:10:08 +0200

    Storybook: Add multi import next/font/google example

--- example/src/app/components/Font/Font.tsx
+++ example/src/app/components/Font/Font.tsx
@@ -1,8 +1,21 @@
 /* eslint-disable react/prop-types */
 import { Comic_Neue } from "next/font/google";
+import { Kalnia, Roboto_Mono } from "next/font/google";
 import localFont from "next/font/local";
 
 import React from "react";
+
+export const kalina = Kalnia({
+	subsets: ["latin"],
+	variable: "--font-kalina",
+	display: "swap",
+});
+
+export const roboto_mono = Roboto_Mono({
+	subsets: ["latin"],
+	variable: "--font-roboto-mono",
+	display: "swap",
+});
 
 const comicNeue = Comic_Neue({
 	subsets: ["latin"],
@@ -24,6 +37,8 @@ export default function Font({ variant }: FontProps) {
 		case "className":
 			return (
 				<div>
+					<h1 className={kalina.className}>Google Kalina</h1>
+					<h1 className={roboto_mono.className}>Google Roboto Mono</h1>
 					<h1 className={comicNeue.className}>Google Comic Neue</h1>
 					<h1 className={localRubikStorm.className}>Local Rubik Storm</h1>
 				</div>
@@ -31,6 +46,8 @@ export default function Font({ variant }: FontProps) {
 		case "style":
 			return (
 				<div>
+					<h1 style={kalina.style}>Google Kalina</h1>
+					<h1 style={roboto_mono.style}>Google Roboto Mono</h1>
 					<h1 style={comicNeue.style}>Google Comic Neue</h1>
 					<h1 style={localRubikStorm.style}>Local Rubik Storm</h1>
 				</div>
@@ -38,6 +55,28 @@ export default function Font({ variant }: FontProps) {
 		case "variable":
 			return (
 				<div>
+					<div className={kalina.variable}>
+						<h1
+							style={{
+								fontFamily: "var(--font-kalina)",
+								fontStyle: kalina.style.fontStyle,
+								fontWeight: kalina.style.fontWeight,
+							}}
+						>
+							Google Kalina
+						</h1>
+					</div>
+					<div className={roboto_mono.variable}>
+						<h1
+							style={{
+								fontFamily: "var(--font-roboto-mono)",
+								fontStyle: roboto_mono.style.fontStyle,
+								fontWeight: roboto_mono.style.fontWeight,
+							}}
+						>
+							Google Roboto Mono
+						</h1>
+					</div>
 					<div className={comicNeue.variable}>
 						<h1
 							style={{
