d1f972ba662043f98215ed1784ddcc87b547d077
Yann Braga <yannbf@gmail.com>
2024-07-24 14:15:12 +0200

    set up CI checks

--- example/vitest.config.ts
+++ example/vitest.config.ts
@@ -2,6 +2,7 @@ import vitePluginNext from "vite-plugin-next";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+	// @ts-expect-error TODO fix this later
 	plugins: [vitePluginNext()],
 	test: {
 		globals: true,
