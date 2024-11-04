ea692b344c3fa44fb6d8c10568b9aaba83232350
Valentin Palkovic <valentin@chromatic.com>
2024-11-04 15:35:47 +0100

    Upgrade to latest Storybook

--- example/vitest.workspace.ts
+++
@@ -1,8 +0,0 @@
-import { defineWorkspace } from "vitest/config";
-
-export default defineWorkspace([
-  // Next.js tests (non-storybook ones), run in isolation via `pnpm run test`
-  "./vitest.config.mts",
-  // Storybook portable stories test, run in isolation via `pnpm run test:storybook`
-  "./.storybook/vitest.config.mts",
-]);
