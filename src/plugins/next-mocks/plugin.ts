ce898c25e26b42b9d6fc342862a467aa9ff0f40a
Valentin Palkovic <valentin@chromatic.com>
2025-01-22 15:33:17 +0100

    fix: Add styled-jsx/style alias to next-mocks plugin

--- src/plugins/next-mocks/plugin.ts
+++ src/plugins/next-mocks/plugin.ts
@@ -1,4 +1,5 @@
 import { createRequire } from "node:module";
+import path, { resolve } from "node:path";
 import type { Plugin } from "vite";
 import { VITEST_PLUGIN_NAME, getExecutionEnvironment } from "../../utils";
 
@@ -39,6 +40,9 @@ export const getAlias = (env: Env) => ({
   "@opentelemetry/api": require.resolve(
     "next/dist/compiled/@opentelemetry/api",
   ),
+  "styled-jsx/style.js": resolve("styled-jsx/style"),
+  "styled-jsx/style": require.resolve("styled-jsx/style"),
+  "styled-jsx": path.dirname(require.resolve("styled-jsx/package.json")),
   "next/dynamic": getEntryPoint("dynamic", env),
 });
 
