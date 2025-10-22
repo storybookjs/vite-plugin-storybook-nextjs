ddf2350555514d91a6e7df2d081c2debc9e7e489
Yann Braga <yannbf@gmail.com>
2025-10-22 17:57:53 +0200

    add new exports and alias safe-stable-stringify package

--- src/plugins/next-mocks/alias/cache/index.ts
+++ src/plugins/next-mocks/alias/cache/index.ts
@@ -14,19 +14,30 @@ const revalidatePath: Mock<Procedure> = fn().mockName(
 const revalidateTag: Mock<Procedure> = fn().mockName(
   "next/cache::revalidateTag",
 );
+const updateTag: Mock<Procedure> = fn().mockName("next/cache::updateTag");
 const unstable_cache: Mock<Procedure> = fn()
   .mockName("next/cache::unstable_cache")
   .mockImplementation((cb: Callback) => cb);
 const unstable_noStore: Mock<Procedure> = fn().mockName(
   "next/cache::unstable_noStore",
 );
+const refresh: Mock<Procedure> = fn().mockName("next/cache::refresh");
 
 const cacheExports = {
   unstable_cache,
   revalidateTag,
   revalidatePath,
+  updateTag,
+  refresh,
   unstable_noStore,
 };
 
 export default cacheExports;
-export { unstable_cache, revalidateTag, revalidatePath, unstable_noStore };
+export {
+  unstable_cache,
+  revalidateTag,
+  revalidatePath,
+  unstable_noStore,
+  refresh,
+  updateTag,
+};
