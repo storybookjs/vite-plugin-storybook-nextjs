f205d5e4ceb23487d03a2a1fcac1068790899b31
k35o <kouki_sakano@dwango.co.jp>
2026-04-22 18:20:05 +0900

    Handle ambiguous default import from CJS module in next/image mocks

--- src/plugins/next-image/alias/next-image.tsx
+++ src/plugins/next-image/alias/next-image.tsx
@@ -7,8 +7,18 @@ import { ImageContext } from "sb-original/image-context";
 
 import React from "next/dist/compiled/react";
 
-const OriginalNextImage = NextImageNamespace.default;
-const { getImageProps: originalGetImageProps } = NextImageNamespace;
+// Handle ambiguous default import from CJS module.
+// See: https://rolldown.rs/in-depth/bundling-cjs#recommendations-for-library-authors
+const _raw = NextImageNamespace.default;
+const OriginalNextImage =
+  typeof _raw === "object" && _raw !== null && "__esModule" in _raw
+    ? (_raw as Record<string, typeof _raw>).default
+    : _raw;
+const originalGetImageProps =
+  NextImageNamespace.getImageProps ??
+  (typeof _raw === "object" && _raw !== null && "getImageProps" in _raw
+    ? (_raw as Record<string, typeof NextImageNamespace.getImageProps>).getImageProps
+    : undefined);
 
 const MockedNextImage = React.forwardRef<
   HTMLImageElement,
--- src/plugins/next-image/alias/next-legacy-image.tsx
+++ src/plugins/next-image/alias/next-legacy-image.tsx
@@ -1,10 +1,18 @@
 // @ts-ignore import is aliased in webpack config
-import OriginalNextLegacyImage from "next/legacy/image";
+import * as _NextLegacyImageNamespace from "next/legacy/image";
 import { defaultLoader } from "sb-original/default-loader";
 import { ImageContext } from "sb-original/image-context";
 
 import React from "next/dist/compiled/react";
 import type * as _NextLegacyImage from "next/legacy/image";
+
+// Handle ambiguous default import from CJS module.
+// See: https://rolldown.rs/in-depth/bundling-cjs#recommendations-for-library-authors
+const _rawLegacy = _NextLegacyImageNamespace.default;
+const OriginalNextLegacyImage =
+  typeof _rawLegacy === "object" && _rawLegacy !== null && "__esModule" in _rawLegacy
+    ? (_rawLegacy as Record<string, typeof _rawLegacy>).default
+    : _rawLegacy;
 
 function NextLegacyImage({ loader, ...props }: _NextLegacyImage.ImageProps) {
   const imageParameters = React.useContext(ImageContext);
